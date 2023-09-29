0f5b5e85b9370ac07c479df8af3e6025cbcf52c8
Sam Leung <sam.leung@lldevgt-sleung2.office.globalrelay.net>
2023-09-29 14:02:44 -0700

    VA-6189 add generate string method

--- helper/string-utils.ts
+++ helper/string-utils.ts
@@ -1,8 +1,29 @@
+import { randomBytes } from 'crypto';
 
 export class StringUtils {
-
     generateString(min = 2, max = 15) {
-        return Math.random().toString(36).substring(min, max) + Math.random().toString(36).substring(min, max);
+        return (
+            Math.random().toString(36).substring(min, max) + Math.random().toString(36).substring(min, max)
+        );
     }
 
+    static generateStringbyBytes(bytes = 8) {
+        const resultBytes = randomBytes(bytes);
+        const resultString = resultBytes.toString('hex');
+        return resultString;
+    }
+
+    static randomInt(min: number, max: number) {
+        // The maximum is inclusive and the minimum is inclusive
+        const randInt = Math.floor(Math.random() * (max - min + 1)) + min;
+        return String(randInt);
+    }
+
+    static randomPhone() {
+        const prefix = this.randomInt(2, 9) + this.randomInt(0, 9) + this.randomInt(0, 9);
+        const mid = this.randomInt(2, 9) + this.randomInt(0, 9) + this.randomInt(0, 9);
+        const end = this.randomInt(0, 9) + this.randomInt(0, 9) + this.randomInt(0, 9) + this.randomInt(0, 9);
+
+        return prefix + mid + end;
+    }
 }
