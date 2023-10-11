622732d9dfbe2b698ee1274b8d672228239874f7
Sam Leung <sam.leung@lldevgt-sleung2.office.globalrelay.net>
2023-10-10 17:28:25 -0700

    VA-6189 remove sm-api and remove endpoints from playwright.config.ts

--- tests/C666.spec.ts
+++ tests/C666.spec.ts
@@ -1,34 +1,20 @@
 import { test } from '@playwright/test';
 import { Company } from 'Apis/company';
-// import { UserUtils } from 'Apis/sm/helpers/user-utils';
+import { User } from 'Apis/user';
+
 test('C666', async () => {
-    const company = new Company();
+    let company1: Company;
+    let user1: User;
     try {
-        // const [user1, user2, user3] = await UserUtils.createCompanyAndUsers(3, company)
-        // await user1.updateJobTitle('test job title')
-        // console.log(await user1.getUserProfile())
-        //     await user1.removeCompanyEntitlement()
-        //     await user1.removeFileSharingEntitlement()
-        //     await user1.removeInstantMessageEntitlement()
-        //     await user1.removeManageBusinessChannelsEntitlement()
-        //     await user1.removeManageCompanyChannelsEntitlement()
-        //     await user1.removeMessageApplicationEntitlement()
-        //     await user1.removePublicEntitlement()
-        //     await user1.addCompanyEntitlement()
-        //     await user1.addFileSharingEntitlement()
-        //     await user1.addInstantMessageEntitlement()
-        //     await user1.addManageBusinessChannelsEntitlement()
-        //     await user1.addManageCompanyChannelsEntitlement()
-        //     await user1.addMessageApplicationEntitlement()
-        //     await user1.addPublicEntitlement()
-        //     await user1.assignMessageAdminRole()
-        //     await user1.assignSMSUserWithCallForwardRole()
-        //     await user1.unassignSMSUserWithCallForwardRole()
-        //     await user1.unassignMessageAdminRole()
+        company1 = await Company.createCompany();
+        user1 = await company1.createUser();
+        await user1.removeEntitlement('FILE_SHARING');
+        await user1.requestAndAssignWhatsAppNumber();
+        await user1.unasssignAndReleaseWhatAppNumber();
     } catch (err) {
         console.error(err);
         throw err;
     } finally {
-        await company.deleteCompany();
+        await company1.deleteCompany();
     }
 });
