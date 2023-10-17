4f865b851255b6b7edae131f16de1d8f59e868ba
Tahmeed Hossain <tahmeed.hossain@globalrelay.net>
2023-10-17 14:09:57 -0700

    push latest

--- tests/1-1-send-receive-download-image-C2597783.spec.ts
+++ tests/1-1-send-receive-download-image-C2597783.spec.ts
@@ -1,41 +1,38 @@
-import { test,chromium} from '@playwright/test';
+import { test, chromium } from '@playwright/test';
+import { Company } from 'Apis/company';
 import { BaseController } from '../controller/base-controller';
-
 
 test.describe('@Smoke @MUC @Local @FileSharing @Image', () => {
     let browser = null;
     let context1 = null;
     let context2 = null;
-    let app : BaseController;
-    let app1 : BaseController;
+    let app: BaseController;
+    let app1: BaseController;
 
-    let createManager : companyCreateManager;
+    let company: Company;
     let user1 = null;
     let user2 = null;
 
     test.beforeEach(async () => {
         browser = await chromium.launch();
-        createManager = new companyCreateManager();
-        const company = await createManager.init(2);
-        user1 = createManager.users[0];
-        user2 = createManager.users[1];
+        company = await Company.createCompany();
+        user1 = await company.createUser();
+        user2 = await company.createUser();
     });
 
     test('@Real C2597783: Send, receive and download image from SUC', async () => {
-        // user1 login 
+        // user1 login
         context1 = await browser.newContext();
         const page1 = await context1.newPage();
         app = new BaseController(page1);
-        await page1.goto(baseURL);
-        
-        // user login 
-        await app.loginController.loginToPortal(user1.email, user1.password);
+        await app.goToLoginPage();
+        // user login
+        await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
         await app.closeTooltips();
-        
+
         // user start 1-1
         await app.startChatButtonController.ClickOnStartOneToOne();
-        await app.createChatController.CreateSUC(user2.firstName);
-
+        await app1.createChatController.CreateSUC(user1.userInfo.lastName);
         // user start conversation with user 2
         const randomContent = app.stringUtils.generateString();
         await app.chatController.sendContent(randomContent);
@@ -51,18 +48,17 @@ test.describe('@Smoke @MUC @Local @FileSharing @Image', () => {
         // user 2 accept invitation with user 1
         await app1.startChatButtonController.ClickOnStartOneToOne();
         await app1.createChatController.CreateSUC(user1.firstName);
-        await app1.inviteController.acceptInvite("SUC");
+        await app1.inviteController.acceptInvite('SUC');
 
         // user send image in conversation
         const PNG = './asset/download.png';
         await app.chatController.waitForHeader();
         await app.attachmentController.sendAttachment(PNG);
-    
+
         // user 2 download image
-        await app1.chatController.waitForHeader()
+        await app1.chatController.waitForHeader();
         await app1.chatController.downloadLastMedia();
         await page2.waitForEvent('download');
-
     });
 
     test.afterEach(async () => {
@@ -72,5 +68,4 @@ test.describe('@Smoke @MUC @Local @FileSharing @Image', () => {
         await context2.close();
         await createManager.cleanup();
     });
-
-})
+});
