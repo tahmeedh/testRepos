6cc03f439d986d44c52c57fa183bac43fd2f33b6
Tahmeed Hossain <tahmeed.hossain@globalrelay.net>
2023-10-31 10:42:00 -0700

    remove unnesscary poms

--- poms/chat-list-page.ts
+++ poms/chat-list-page.ts
@@ -3,23 +3,15 @@ import { BasePage } from './base-page';
 
 export class ChatListPage extends BasePage {
     readonly page: Page;
-    readonly CHAT_BODY_PARTIAL: Locator;
-    readonly CHAT_ITEM_ROW: Locator;
-    readonly AVATAR_CONTAINER: Locator;
-    readonly CHAT_LIST_CONTAINER: Locator;
-    readonly CONVERSATION_LIST_CONTAINER: Locator;
-    readonly CHANNEL_LIST_CONTAINER: Locator;
-    readonly CONVERSATIONS_CONTAINER: Locator;
     readonly CHAT_CONTAINER: Locator;
     readonly CHANNELS_CONTAINER: Locator;
     readonly CHATS_BUTTON: Locator;
     readonly CONTACTS_BUTTON: Locator;
 
     readonly CHAT_NAME: Locator;
-    readonly SECONDARY_LINE: Locator;
-    readonly CHANNEL_DRAFT_LINE: Locator;
-    readonly N_BADGE: Locator;
-    readonly FAVOURITE_ICON: Locator;
+    readonly DRAFT_TEXT_LINE: Locator;
+    readonly ATTACHMENT_ICON: Locator;
+    readonly ATTACHMENT_TEXT_LINE: Locator;
     readonly CHANNEL_INFO: Locator;
     readonly INVITE_BADGE: Locator;
     readonly CHAT_ROWS: Locator;
@@ -28,33 +20,14 @@ export class ChatListPage extends BasePage {
     constructor(page: Page) {
         super(page);
 
-        // Partials
-        this.CHAT_BODY_PARTIAL = this.CHATIFRAME.locator('.list-item-body');
-        this.CHAT_ITEM_ROW = this.CHATIFRAME.locator('.m-auto-list-item-row');
-
-        // Containers
-        this.AVATAR_CONTAINER = this.CHATIFRAME.locator('.m-auto-avatar-container');
-        this.CHAT_LIST_CONTAINER = this.CHATIFRAME.locator('.m-auto-accordion');
-        this.CONVERSATION_LIST_CONTAINER = this.CHATIFRAME.locator('.conversation-list-container');
-        this.CHANNEL_LIST_CONTAINER = this.CHATIFRAME.locator('.channel-list-container');
-        this.CHANNELS_CONTAINER = this.CHATIFRAME.locator('.m-auto-accordion-item-channels');
-        this.CHAT_ROWS = this.CHATIFRAME.locator('.conversation-list-container .m-auto-list-item-row');
-        this.CHANNEL_ROWS = this.CHATIFRAME.locator('.channel-list-container .m-auto-list-item-row');
-
         //sidebar
         this.CHATS_BUTTON = this.MESSAGEIFRAME.locator('.m-auto-chats-button');
         this.CONTACTS_BUTTON = this.CHATIFRAME.locator('.m-auto-contacts-button');
 
         // Row Information
         this.CHAT_NAME = this.MESSAGEIFRAME.locator('.twemoji-wrapper');
-        this.SECONDARY_LINE = this.CHATIFRAME.locator('.m-auto-secondary-line');
-        this.CHANNEL_DRAFT_LINE = this.CHATIFRAME.locator('.m-auto-list-item-draft');
-
-        this.N_BADGE = this.CHATIFRAME.locator('.m-auto-blue-badge');
-        this.FAVOURITE_ICON = this.CHATIFRAME.locator('.m-auto-favourite-icon');
-        this.INVITE_BADGE = this.CHATIFRAME.locator('.m-auto-has-new-invite');
-        // -- Row Channels
-
-        this.CHANNEL_INFO = this.CHATIFRAME.locator('.m-auto-list-item-row');
+        this.DRAFT_TEXT_LINE = this.MESSAGEIFRAME.locator('.list-item-text-draft');
+        this.ATTACHMENT_ICON = this.MESSAGEIFRAME.locator('.list-item-text-icon');
+        this.ATTACHMENT_TEXT_LINE = this.MESSAGEIFRAME.getByText('Attachment');
     }
 }
