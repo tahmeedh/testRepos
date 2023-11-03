c5df1bfa32ace59af4e90497f93a6c682db4b18c
Tahmeed Hossain <tahmeed.hossain@globalrelay.net>
2023-11-03 11:45:36 -0700

    add test utils to each test

--- tests/message-hub/channels/open/C3793179-open-channel-draft-has-attachment-label-for-unsent-files.spec.ts
+++ tests/message-hub/channels/open/C3793179-open-channel-draft-has-attachment-label-for-unsent-files.spec.ts
@@ -1,58 +1,59 @@
 import { expect, test, chromium } from '@playwright/test';
 import { Company } from 'Apis/company';
 import { StringUtils } from 'helper/string-utils';
+import { TestUtils } from 'helper/test-utils';
 import { BaseController } from '../../../../controller/base-controller';
 
-test.describe('@Open @Channel @Draft', () => {
-    let browser = null;
-    let context1 = null;
-    let app = null;
-    let company: Company;
-    let user1 = null;
-    let user2 = null;
+const { testAnnotation, testName, testTags } = TestUtils.getTestInfo(__filename);
+let browser = null;
+let context1 = null;
+let app = null;
+let company: Company;
+let user1 = null;
+let user2 = null;
 
-    test.beforeEach(async () => {
-        browser = await chromium.launch();
-        company = await Company.createCompany();
-        user1 = await company.createUser();
-        user2 = await company.createUser();
-        await company.addUserToEachOthersRoster([user1, user2]);
-    });
+test.beforeEach(async () => {
+    browser = await chromium.launch();
+    company = await Company.createCompany();
+    user1 = await company.createUser();
+    user2 = await company.createUser();
+    await company.addUserToEachOthersRoster([user1, user2]);
+});
 
-    test('@Real C3793179 : Open Channel draft state has file attachment icon and text for unsent files ', async () => {
-        // user1 login
-        context1 = await browser.newContext();
-        const page1 = await context1.newPage();
-        app = new BaseController(page1);
-        await app.goToLoginPage();
-        await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
-        await app.closeTooltips();
+test(`${testName} ${testTags}`, async () => {
+    test.info().annotations.push(testAnnotation);
+    // user1 login
+    context1 = await browser.newContext();
+    const page1 = await context1.newPage();
+    app = new BaseController(page1);
+    await app.goToLoginPage();
+    await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
+    await app.closeTooltips();
 
-        // user create channel
-        await app.startChatButtonController.ClickOnStartChannel();
-        const title = StringUtils.generateString(3, 5);
-        await app.createChatController.fillOutWhatIsItAboutForm(title, 'sub', 'descri');
-        await app.createChatController.fillOutWhoCanPostForm();
-        await app.createChatController.fillOutWhoCanJoinForm(
-            'open',
-            [],
-            [`${user2.userInfo.firstName} ${user2.userInfo.lastName}`]
-        );
-        await app.createChatController.CreateChannel();
-        await app.chatController.sendContent();
+    // user create channel
+    await app.startChatButtonController.ClickOnStartChannel();
+    const title = StringUtils.generateString(3, 5);
+    await app.createChatController.fillOutWhatIsItAboutForm(title, 'sub', 'descri');
+    await app.createChatController.fillOutWhoCanPostForm();
+    await app.createChatController.fillOutWhoCanJoinForm(
+        'open',
+        [],
+        [`${user2.userInfo.firstName} ${user2.userInfo.lastName}`]
+    );
+    await app.createChatController.CreateChannel();
+    await app.chatController.sendContent();
 
-        // user drafts image in conversation
-        const PNG = './asset/download.png';
-        await app.chatController.waitForHeader();
-        await app.attachmentController.draftAttachment(PNG);
-        await app.messageHubController.clickSideBarChatsButton();
+    // user drafts image in conversation
+    const PNG = './asset/download.png';
+    await app.chatController.waitForHeader();
+    await app.attachmentController.draftAttachment(PNG);
+    await app.messageHubController.clickSideBarChatsButton();
 
-        expect(app.messageHubController.Pom.DRAFT_TEXT_LINE).toBeVisible();
-        expect(app.messageHubController.Pom.ATTACHMENT_ICON).toBeVisible();
-        expect(app.messageHubController.Pom.ATTACHMENT_TEXT_LINE).toBeVisible();
-    });
+    expect(app.messageHubController.Pom.DRAFT_TEXT_LINE).toBeVisible();
+    expect(app.messageHubController.Pom.ATTACHMENT_ICON).toBeVisible();
+    expect(app.messageHubController.Pom.ATTACHMENT_TEXT_LINE).toBeVisible();
+});
 
-    test.afterEach(async () => {
-        await company.teardown();
-    });
+test.afterEach(async () => {
+    await company.teardown();
 });
--- tests/message-hub/channels/restricted/C3792876-restricted-channel-draft-state-correct-elem-text.spec.ts
+++ tests/message-hub/channels/restricted/C3792876-restricted-channel-draft-state-correct-elem-text.spec.ts
@@ -1,54 +1,55 @@
 import { expect, test, chromium } from '@playwright/test';
 import { Company } from 'Apis/company';
 import { StringUtils } from 'helper/string-utils';
+import { TestUtils } from 'helper/test-utils';
 import { BaseController } from '../../../../controller/base-controller';
 
-test.describe('@Restricted @Channel @Draft', () => {
-    let browser = null;
-    let context1 = null;
-    let app = null;
-    let company: Company;
-    let user1 = null;
-    let user2 = null;
+const { testAnnotation, testName, testTags } = TestUtils.getTestInfo(__filename);
+let browser = null;
+let context1 = null;
+let app = null;
+let company: Company;
+let user1 = null;
+let user2 = null;
 
-    test.beforeEach(async () => {
-        browser = await chromium.launch();
-        company = await Company.createCompany();
-        user1 = await company.createUser();
-        user2 = await company.createUser();
-        await company.addUserToEachOthersRoster([user1, user2]);
-    });
+test.beforeEach(async () => {
+    browser = await chromium.launch();
+    company = await Company.createCompany();
+    user1 = await company.createUser();
+    user2 = await company.createUser();
+    await company.addUserToEachOthersRoster([user1, user2]);
+});
 
-    test('@Real C2599572 : Restricted channel displays correct elements of draft state for unsent message', async () => {
-        // user1 login
-        context1 = await browser.newContext();
-        const page1 = await context1.newPage();
-        app = new BaseController(page1);
-        await app.goToLoginPage();
-        await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
-        await app.closeTooltips();
+test(`${testName} ${testTags}`, async () => {
+    test.info().annotations.push(testAnnotation);
+    // user1 login
+    context1 = await browser.newContext();
+    const page1 = await context1.newPage();
+    app = new BaseController(page1);
+    await app.goToLoginPage();
+    await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
+    await app.closeTooltips();
 
-        // user create channel
-        await app.startChatButtonController.ClickOnStartChannel();
-        const title = StringUtils.generateString(3, 5);
-        await app.createChatController.fillOutWhatIsItAboutForm(title, 'sub', 'descri');
-        await app.createChatController.fillOutWhoCanPostForm();
-        await app.createChatController.fillOutWhoCanJoinForm(
-            'restricted',
-            [],
-            [`${user2.userInfo.firstName} ${user2.userInfo.lastName}`]
-        );
-        await app.createChatController.CreateChannel();
-        const draftText = StringUtils.generateString();
-        await app.chatController.sendContent();
-        await app.chatController.typeContent(draftText);
-        await app.messageHubController.clickSideBarChatsButton();
+    // user create channel
+    await app.startChatButtonController.ClickOnStartChannel();
+    const title = StringUtils.generateString(3, 5);
+    await app.createChatController.fillOutWhatIsItAboutForm(title, 'sub', 'descri');
+    await app.createChatController.fillOutWhoCanPostForm();
+    await app.createChatController.fillOutWhoCanJoinForm(
+        'restricted',
+        [],
+        [`${user2.userInfo.firstName} ${user2.userInfo.lastName}`]
+    );
+    await app.createChatController.CreateChannel();
+    const draftText = StringUtils.generateString();
+    await app.chatController.sendContent();
+    await app.chatController.typeContent(draftText);
+    await app.messageHubController.clickSideBarChatsButton();
 
-        const secondaryLine = await app.Pom.MESSAGEIFRAME.getByText(draftText);
-        await expect(secondaryLine).toHaveText(draftText);
-    });
+    const secondaryLine = await app.Pom.MESSAGEIFRAME.getByText(draftText);
+    await expect(secondaryLine).toHaveText(draftText);
+});
 
-    test.afterEach(async () => {
-        await company.teardown();
-    });
+test.afterEach(async () => {
+    await company.teardown();
 });
