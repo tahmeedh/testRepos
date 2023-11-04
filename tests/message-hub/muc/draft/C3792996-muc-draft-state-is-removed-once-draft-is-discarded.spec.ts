dbbbbdad8dcae90a2f14745020e9e5b66451fc19
Tahmeed Hossain <tahmeed.hossain@globalrelay.net>
2023-11-04 15:22:33 -0700

    add test util func for all

--- tests/message-hub/muc/draft/C3792996-muc-draft-state-is-removed-once-draft-is-discarded.spec.ts
+++ tests/message-hub/muc/draft/C3792996-muc-draft-state-is-removed-once-draft-is-discarded.spec.ts
@@ -1,58 +1,59 @@
 import { test, expect, chromium } from '@playwright/test';
 import { Company } from 'Apis/company';
+import { TestUtils } from 'helper/test-utils';
 import { BaseController } from '../../../../controller/base-controller';
 import { StringUtils } from '../../../../helper/string-utils';
 
-test.describe('@MUC @Draft', () => {
-    let browser = null;
-    let context1 = null;
-    let app: BaseController;
+const { testAnnotation, testName, testTags } = TestUtils.getTestInfo(__filename);
+let browser = null;
+let context1 = null;
+let app: BaseController;
 
-    let company: Company;
-    let user1 = null;
-    let user2 = null;
-    let user3 = null;
+let company: Company;
+let user1 = null;
+let user2 = null;
+let user3 = null;
 
-    test.beforeEach(async () => {
-        browser = await chromium.launch();
-        company = await Company.createCompany();
-        user1 = await company.createUser();
-        user2 = await company.createUser();
-        user3 = await company.createUser();
-        await company.addUserToEachOthersRoster([user1, user2]);
-    });
+test.beforeEach(async () => {
+    browser = await chromium.launch();
+    company = await Company.createCompany();
+    user1 = await company.createUser();
+    user2 = await company.createUser();
+    user3 = await company.createUser();
+    await company.addUserToEachOthersRoster([user1, user2]);
+});
 
-    test('@Real C3792981: MUC draft state is removed from Message Hub after chat input is discarded ', async () => {
-        // user1 login
-        context1 = await browser.newContext();
-        const page1 = await context1.newPage();
-        app = new BaseController(page1);
+test(`${testName} ${testTags}`, async () => {
+    test.info().annotations.push(testAnnotation);
+    // user1 login
+    context1 = await browser.newContext();
+    const page1 = await context1.newPage();
+    app = new BaseController(page1);
 
-        await app.goToLoginPage();
-        // user login
-        await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
-        await app.closeTooltips();
+    await app.goToLoginPage();
+    // user login
+    await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
+    await app.closeTooltips();
 
-        // user start 1-1
-        await app.startChatButtonController.ClickOnStartMUC();
-        const user2fullName = `${user2.userInfo.firstName} ${user2.userInfo.lastName}`;
-        const user3fullName = `${user3.userInfo.firstName} ${user3.userInfo.lastName}`;
-        const subject = await app.createChatController.createMUC([user2fullName, user3fullName]);
+    // user start 1-1
+    await app.startChatButtonController.ClickOnStartMUC();
+    const user2fullName = `${user2.userInfo.firstName} ${user2.userInfo.lastName}`;
+    const user3fullName = `${user3.userInfo.firstName} ${user3.userInfo.lastName}`;
+    const subject = await app.createChatController.createMUC([user2fullName, user3fullName]);
 
-        // user send message in conversation
-        const draftText = StringUtils.generateString();
-        await app.chatController.sendContent();
-        await app.chatController.typeContent(draftText);
-        await app.messageHubController.clickSideBarChatsButton();
+    // user send message in conversation
+    const draftText = StringUtils.generateString();
+    await app.chatController.sendContent();
+    await app.chatController.typeContent(draftText);
+    await app.messageHubController.clickSideBarChatsButton();
 
-        await app.messageHubController.clickMessageHubRow(subject);
-        await app.chatController.removeContent();
-        await app.messageHubController.clickSideBarChatsButton();
-        const secondaryLine = await app.Pom.MESSAGEIFRAME.getByText(draftText);
-        await expect(secondaryLine).toHaveCount(0);
-    });
+    await app.messageHubController.clickMessageHubRow(subject);
+    await app.chatController.removeContent();
+    await app.messageHubController.clickSideBarChatsButton();
+    const secondaryLine = await app.Pom.MESSAGEIFRAME.getByText(draftText);
+    await expect(secondaryLine).toHaveCount(0);
+});
 
-    test.afterEach(async () => {
-        await company.teardown();
-    });
+test.afterEach(async () => {
+    await company.teardown();
 });
--- tests/message-hub/sms/C3792997-sms-draft-state-is-removed-once-draft-is-discarded.spec.ts
+++ tests/message-hub/sms/C3792997-sms-draft-state-is-removed-once-draft-is-discarded.spec.ts
@@ -1,58 +1,59 @@
 import { test, expect, chromium } from '@playwright/test';
 import { Company } from 'Apis/company';
+import { TestUtils } from 'helper/test-utils';
 import { BaseController } from '../../../controller/base-controller';
 import { StringUtils } from '../../../helper/string-utils';
 
-test.describe('@SMS @Draft', () => {
-    let browser = null;
-    let context1 = null;
-    let app: BaseController;
+const { testAnnotation, testName, testTags } = TestUtils.getTestInfo(__filename);
+let browser = null;
+let context1 = null;
+let app: BaseController;
 
-    let company: Company;
-    let user1 = null;
+let company: Company;
+let user1 = null;
 
-    test.beforeEach(async () => {
-        browser = await chromium.launch();
-        company = await Company.createCompany();
-        user1 = await company.createUser();
+test.beforeEach(async () => {
+    browser = await chromium.launch();
+    company = await Company.createCompany();
+    user1 = await company.createUser();
 
-        await Promise.all([
-            user1.assignServiceManagerRole('MESSAGE_ADMINISTRATOR'),
-            user1.assignDirectoryRole('SMS_USER_WITH_CALL_FORWARD')
-        ]);
+    await Promise.all([
+        user1.assignServiceManagerRole('MESSAGE_ADMINISTRATOR'),
+        user1.assignDirectoryRole('SMS_USER_WITH_CALL_FORWARD')
+    ]);
 
-        await user1.requestAndAssignTwilioNumber();
-    });
+    await user1.requestAndAssignTwilioNumber();
+});
 
-    test('@Real C3792982: Conversation list displays correct elements of SMS draft state for unsent message', async () => {
-        // user1 login
-        context1 = await browser.newContext();
-        const page1 = await context1.newPage();
-        app = new BaseController(page1);
+test(`${testName} ${testTags}`, async () => {
+    test.info().annotations.push(testAnnotation);
+    // user1 login
+    context1 = await browser.newContext();
+    const page1 = await context1.newPage();
+    app = new BaseController(page1);
 
-        await app.goToLoginPage();
-        // user login
-        await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
-        await app.closeTooltips();
+    await app.goToLoginPage();
+    // user login
+    await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
+    await app.closeTooltips();
 
-        // user start 1-1
-        await app.startChatButtonController.ClickOnStartSMS();
-        const randonNumber = await app.createChatController.CreateSMS();
-        await app.chatController.skipRecipientInfo();
-        // user send message in conversation
-        const draftText = StringUtils.generateString();
-        await app.chatController.sendContent();
-        await app.chatController.typeContent(draftText);
-        await app.messageHubController.clickSideBarChatsButton();
+    // user start 1-1
+    await app.startChatButtonController.ClickOnStartSMS();
+    const randonNumber = await app.createChatController.CreateSMS();
+    await app.chatController.skipRecipientInfo();
+    // user send message in conversation
+    const draftText = StringUtils.generateString();
+    await app.chatController.sendContent();
+    await app.chatController.typeContent(draftText);
+    await app.messageHubController.clickSideBarChatsButton();
 
-        await app.messageHubController.clickMessageHubRow(randonNumber);
-        await app.chatController.removeContent();
-        await app.messageHubController.clickSideBarChatsButton();
-        const secondaryLine = await app.Pom.MESSAGEIFRAME.getByText(draftText);
-        await expect(secondaryLine).toHaveCount(0);
-    });
+    await app.messageHubController.clickMessageHubRow(randonNumber);
+    await app.chatController.removeContent();
+    await app.messageHubController.clickSideBarChatsButton();
+    const secondaryLine = await app.Pom.MESSAGEIFRAME.getByText(draftText);
+    await expect(secondaryLine).toHaveCount(0);
+});
 
-    test.afterEach(async () => {
-        await company.teardown();
-    });
+test.afterEach(async () => {
+    await company.teardown();
 });
