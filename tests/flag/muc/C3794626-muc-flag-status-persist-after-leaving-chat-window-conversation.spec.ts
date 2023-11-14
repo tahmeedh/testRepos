5934d2936a2cf8b3793bb3da17d90aa39d54aba5
Rhythm <rhythm.vashit@globalrelay.net>
2023-11-14 09:43:36 -0800

    CA-925  Used test utils

--- tests/flag/muc/C3794626-muc-flag-status-persist-after-leaving-chat-window-conversation.spec.ts
+++ tests/flag/muc/C3794626-muc-flag-status-persist-after-leaving-chat-window-conversation.spec.ts
@@ -1,77 +1,78 @@
 import { test, chromium, expect } from '@playwright/test';
 import { Company } from 'Apis/company';
+import { TestUtils } from 'helper/test-utils';
 import { BaseController } from '../../../controller/base-controller';
 import { StringUtils } from '../../../helper/string-utils';
 
-test.describe('@MUC @Flag', () => {
-    let browser = null;
-    let context1 = null;
-    let context2 = null;
-    let app: BaseController;
-    let app1: BaseController;
+const { testAnnotation, testName, testTags } = TestUtils.getTestInfo(__filename);
+let browser = null;
+let context1 = null;
+let context2 = null;
+let app: BaseController;
+let app1: BaseController;
 
-    let company: Company;
-    let user1 = null;
-    let user2 = null;
-    let user3 = null;
+let company: Company;
+let user1 = null;
+let user2 = null;
+let user3 = null;
 
-    test.beforeEach(async () => {
-        browser = await chromium.launch();
-        company = await Company.createCompany();
-        user1 = await company.createUser();
-        user2 = await company.createUser();
-        user3 = await company.createUser();
-        await company.addUserToEachOthersRoster([user1, user2]);
-    });
+test.beforeEach(async () => {
+    browser = await chromium.launch();
+    company = await Company.createCompany();
+    user1 = await company.createUser();
+    user2 = await company.createUser();
+    user3 = await company.createUser();
+    await company.addUserToEachOthersRoster([user1, user2]);
+});
 
-    test(' C3794626  MUC flag status should persist after leaving chat window - Conversation', async () => {
-        // user1 login
-        context1 = await browser.newContext();
-        context2 = await browser.newContext();
-        const page1 = await context1.newPage();
-        app = new BaseController(page1);
-        await app.goToLoginPage();
+test(`${testName} ${testTags}`, async () => {
+    test.info().annotations.push(testAnnotation);
+    // user1 login
+    context1 = await browser.newContext();
+    context2 = await browser.newContext();
+    const page1 = await context1.newPage();
+    app = new BaseController(page1);
+    await app.goToLoginPage();
 
-        // user login
-        await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
-        await app.closeTooltips();
+    // user login
+    await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
+    await app.closeTooltips();
 
-        // user create MUC
-        await app.startChatButtonController.ClickOnStartMUC();
-        const title = StringUtils.generateString(3, 5);
-        const user2fullName = `${user2.userInfo.firstName} ${user2.userInfo.lastName}`;
-        const user3fullName = `${user3.userInfo.firstName} ${user3.userInfo.lastName}`;
-        await app.createChatController.createMUC([user2fullName, user3fullName], title);
+    // user create MUC
+    await app.startChatButtonController.ClickOnStartMUC();
+    const title = StringUtils.generateString(3, 5);
+    const user2fullName = `${user2.userInfo.firstName} ${user2.userInfo.lastName}`;
+    const user3fullName = `${user3.userInfo.firstName} ${user3.userInfo.lastName}`;
+    await app.createChatController.createMUC([user2fullName, user3fullName], title);
 
-        // user send message in MUC
-        const randomContent = StringUtils.generateString();
-        await app.chatController.sendContent(randomContent);
+    // user send message in MUC
+    const randomContent = StringUtils.generateString();
+    await app.chatController.sendContent(randomContent);
 
-        // user 2 login
-        context2 = await browser.newContext();
-        const page2 = await context2.newPage();
-        app1 = new BaseController(page2);
-        await app1.goToLoginPage();
-        await app1.loginController.loginToPortal(user2.userInfo.email, user2.userInfo.password);
-        await app1.closeTooltips();
-        await app1.page.pause();
-        await app1.open(title);
+    // user 2 login
+    context2 = await browser.newContext();
+    const page2 = await context2.newPage();
+    app1 = new BaseController(page2);
+    await app1.goToLoginPage();
+    await app1.loginController.loginToPortal(user2.userInfo.email, user2.userInfo.password);
+    await app1.closeTooltips();
+    await app1.page.pause();
+    await app1.open(title);
 
-        await app1.inviteController.acceptInvite('MUC');
-        await app1.chatController.clickChatFlagButton();
-        await app1.messageHubController.clickSideBarChatsButton();
-        // verify that chat is flagged
-        await expect(app1.Pom.CHAT_FLAG_INDICATOR).toBeVisible();
-        // again open the chat again
-        await app1.open(title);
+    await app1.inviteController.acceptInvite('MUC');
+    await app1.chatController.clickChatFlagButton();
+    await app1.messageHubController.clickSideBarChatsButton();
+    // verify that chat is flagged
+    await expect(app1.Pom.CHAT_FLAG_INDICATOR).toBeVisible();
+    // again open the chat again
+    await app1.open(title);
 
-        // unflag chat
-        await app1.chatController.clickChatFlagButton();
-        await app1.messageHubController.clickSideBarChatsButton();
-        await expect(app1.Pom.CHAT_FLAG_INDICATOR).toBeHidden();
-    });
+    // unflag chat
+    await app1.chatController.clickChatFlagButton();
+    await app1.messageHubController.clickSideBarChatsButton();
+    await expect(app1.Pom.CHAT_FLAG_INDICATOR).toBeHidden();
+});
 
-    test.afterEach(async () => {
-        await company.teardown();
-    });
+test.afterEach(async () => {
+    await company.teardown();
 });
--- tests/flag/sms/C3794617-sms-flag-status-persist-after-leaving-chat-window.spec.ts
+++ tests/flag/sms/C3794617-sms-flag-status-persist-after-leaving-chat-window.spec.ts
@@ -1,57 +1,58 @@
 import { test, chromium, expect } from '@playwright/test';
 import { Company } from 'Apis/company';
+import { TestUtils } from 'helper/test-utils';
 import { BaseController } from '../../../controller/base-controller';
 
-test.describe('@SMS @FLag', () => {
-    let browser = null;
-    let context1 = null;
-    let app: BaseController;
+const { testAnnotation, testName, testTags } = TestUtils.getTestInfo(__filename);
+let browser = null;
+let context1 = null;
+let app: BaseController;
 
-    let company: Company;
-    let user1 = null;
+let company: Company;
+let user1 = null;
 
-    test.beforeEach(async () => {
-        browser = await chromium.launch();
-        company = await Company.createCompany();
-        user1 = await company.createUser();
+test.beforeEach(async () => {
+    browser = await chromium.launch();
+    company = await Company.createCompany();
+    user1 = await company.createUser();
 
-        await Promise.all([
-            user1.assignServiceManagerRole('MESSAGE_ADMINISTRATOR'),
-            user1.assignDirectoryRole('SMS_USER_WITH_CALL_FORWARD')
-        ]);
+    await Promise.all([
+        user1.assignServiceManagerRole('MESSAGE_ADMINISTRATOR'),
+        user1.assignDirectoryRole('SMS_USER_WITH_CALL_FORWARD')
+    ]);
 
-        await user1.requestAndAssignTwilioNumber();
-    });
+    await user1.requestAndAssignTwilioNumber();
+});
 
-    test('C3794617: SMS Flag status persist after leaving chat chat-window', async () => {
-        // user1 login
-        context1 = await browser.newContext();
-        const page1 = await context1.newPage();
-        app = new BaseController(page1);
+test(`${testName} ${testTags}`, async () => {
+    test.info().annotations.push(testAnnotation);
+    // user1 login
+    context1 = await browser.newContext();
+    const page1 = await context1.newPage();
+    app = new BaseController(page1);
 
-        await app.goToLoginPage();
-        // user login
-        await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
-        await app.closeTooltips();
+    await app.goToLoginPage();
+    // user login
+    await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
+    await app.closeTooltips();
 
-        // user start 1-1
-        await app.startChatButtonController.ClickOnStartSMS();
-        await app.chatController.skipRecipientInfo();
+    // user start 1-1
+    await app.startChatButtonController.ClickOnStartSMS();
+    await app.chatController.skipRecipientInfo();
 
-        // user send message in conversation
-        await app.chatController.sendContent();
+    // user send message in conversation
+    await app.chatController.sendContent();
 
-        // user 2 flag chat
-        await app.chatController.clickChatFlagButton();
+    // user 2 flag chat
+    await app.chatController.clickChatFlagButton();
 
-        // to verify that flag icon shows up in the message hub
-        await app.chatController.naviagteToMessageHub();
+    // to verify that flag icon shows up in the message hub
+    await app.chatController.naviagteToMessageHub();
 
-        // Verify the flag
-        await expect(app.Pom.CHAT_FLAG_INDICATOR).toBeVisible();
-    });
+    // Verify the flag
+    await expect(app.Pom.CHAT_FLAG_INDICATOR).toBeVisible();
+});
 
-    test.afterEach(async () => {
-        await company.teardown();
-    });
+test.afterEach(async () => {
+    await company.teardown();
 });
