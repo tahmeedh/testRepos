0cdd815c5d04cf4772ba5d5c36439ed6ce2d38cb
tahmeed.hossain <tahmeed.hossain@globalrelay.net>
2023-11-17 17:50:52 -0800

    add muc accept chat invite tests

---
+++ tests/chat-window/muc/accept/C3795335-muc-accepts-invite-opens-chat-window.spec.ts
@@ -0,0 +1,74 @@
+import { test, expect, chromium } from '@playwright/test';
+import { Company } from 'Apis/company';
+import { StringUtils } from 'helper/string-utils';
+import { Log } from 'Apis/api-helpers/log-utils';
+import { TestUtils } from 'helper/test-utils';
+import { BaseController } from '../../../../controller/base-controller';
+
+const { testAnnotation, testName, testTags, testChatType } = TestUtils.getTestInfo(__filename);
+let browser = null;
+let context1 = null;
+let app = null;
+let context2 = null;
+let app1 = null;
+
+let company: Company;
+let user1 = null;
+let user2 = null;
+let user3 = null;
+
+test.beforeEach(async () => {
+    browser = await chromium.launch();
+    company = await Company.createCompany();
+    user1 = await company.createUser();
+    user2 = await company.createUser();
+    user3 = await company.createUser();
+    await company.addUserToEachOthersRoster([user1, user2, user3]);
+});
+
+test(`${testName} ${testTags}`, async () => {
+    test.info().annotations.push(testAnnotation);
+    Log.starDivider(
+        `START TEST: Create browser and login with ${user1.userInfo.firstName} ${user1.userInfo.lastName}`
+    );
+    context1 = await browser.newContext();
+    const page1 = await context1.newPage();
+    app = new BaseController(page1);
+    await app.goToLoginPage();
+    await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
+    await app.closeTooltips();
+
+    Log.info(`Start ${testChatType} chat and send message`);
+    await app.startChatButtonController.ClickOnStartMUC();
+    const title = StringUtils.generateString(3, 5);
+    const user2fullName = `${user2.userInfo.firstName} ${user2.userInfo.lastName}`;
+    const user3fullName = `${user3.userInfo.firstName} ${user3.userInfo.lastName}`;
+    await app.createChatController.createMUC([user2fullName, user3fullName], title);
+
+    const randomContent = StringUtils.generateString();
+    await app.chatController.sendContent(randomContent);
+
+    Log.info(`login with ${user2.userInfo.firstName} ${user2.userInfo.lastName}`);
+    context2 = await browser.newContext();
+    const page2 = await context2.newPage();
+    app1 = new BaseController(page2);
+    await app1.goToLoginPage();
+    await app1.loginController.loginToPortal(user2.userInfo.email, user2.userInfo.password);
+    await app1.closeTooltips();
+
+    Log.info(`${user2.userInfo.firstName} ${user2.userInfo.lastName} accepts invite`);
+    await app1.open(title);
+    await app1.inviteController.acceptInvite('MUC');
+
+    Log.info(`${user2.userInfo.firstName} ${user2.userInfo.lastName} receives message`);
+    const messageReceived = app1.Pom.CHATIFRAME.getByText(randomContent);
+    await expect(messageReceived).toHaveText(randomContent);
+
+    Log.info(`${user2.userInfo.firstName} ${user2.userInfo.lastName} receives system event`);
+    const systemEvent = app1.Pom.CHATIFRAME.getByText('You joined');
+    await expect(systemEvent).toHaveText('You joined');
+});
+
+test.afterEach(async () => {
+    await company.teardown();
+});
