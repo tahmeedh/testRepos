50b3772543c9933b7581f5a317ea56785bbb12b3
Tahmeed Hossain <tahmeed.hossain@globalrelay.net>
2023-12-08 03:05:47 -0800

    add RemoveUserChannel func

--- controllers/details-controller.ts
+++ controllers/details-controller.ts
@@ -1,5 +1,7 @@
 import type { Page } from '@playwright/test';
+import { Log } from 'Apis/api-helpers/log-utils';
 import { DetailsPage } from '../poms/details-page';
+/* eslint-disable no-await-in-loop */
 
 export class DetailsController {
     readonly page: Page;
@@ -14,6 +16,15 @@ export class DetailsController {
     }
 
     async clickMemberRolesButton() {
-        this.Pom.MEMBER_ROLES_BUTTON.click();
+        Log.info(`Click Member and Roles Button`);
+        await this.Pom.MEMBER_ROLES_BUTTON.click();
+    }
+
+    async RemoveUsersFromChannel(participants: string[]) {
+        for (const participant of participants) {
+            Log.info(`Check if ${participant} already in channel`);
+            await this.Pom.PARTICIPANT_ROW_MENU.first().click();
+            await this.Pom.DROP_DOWN_REMOVE.click();
+        }
     }
 }
--- poms/details-page.ts
+++ poms/details-page.ts
@@ -3,10 +3,18 @@ import { BasePage } from './base-page';
 
 export class DetailsPage extends BasePage {
     readonly MEMBER_ROLES_BUTTON: Locator;
+    readonly PARTICIPANT_ROW_MENU: Locator;
+    readonly PARTICIPANT_ROW: Locator;
+    readonly DROP_DOWN_REMOVE: Locator;
+    readonly DROP_DOWN_PROMPT_TO_MODERATOR: Locator;
 
     constructor(page: Page) {
         super(page);
 
         this.MEMBER_ROLES_BUTTON = this.CHATIFRAME.locator('.m-auto-members-roles-button');
+        this.PARTICIPANT_ROW_MENU = this.CHATIFRAME.locator('.participant-menu');
+        this.PARTICIPANT_ROW = this.CHATIFRAME.locator('.m-auto-participant-row');
+        this.DROP_DOWN_REMOVE = this.CHATIFRAME.getByText('Remove');
+        this.DROP_DOWN_PROMPT_TO_MODERATOR = this.CHATIFRAME.getByText('Promote to Moderator');
     }
 }
