c62732b70afac1484154a259aa35d2808d86ab45
Rhythm <rhythm.vashit@globalrelay.net>
2024-01-02 09:54:24 -0800

    CA-1027  removed unwanted lines

--- poms/details-page.ts
+++ poms/details-page.ts
@@ -17,7 +17,6 @@ export class DetailsPage extends BasePage {
         this.CHAT_SUBJECT_EDIT_FIELD = this.CHATIFRAME.locator(
             'xpath=//*[@id="root"]/div[2]/div[2]/div/div[1]/div/div[2]/div[1]/div/div/div/div/div/div/div'
         );
-        // this.SUBJECT_EDIT_FIELD = this.CHATIFRAME.getByPlaceholder('Enter subject');
         this.SUBJECT_EDIT_FIELD = this.CHATIFRAME.locator(
             'xpath=//*[@id="root"]/div[2]/div/div/div[1]/div/div[2]/div[1]/div/div/div[2]/div'
         );
