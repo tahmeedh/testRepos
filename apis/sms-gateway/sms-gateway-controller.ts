69ac71d392944d8c4c35b78d88942cbba91fce49
Sam Leung <sam.leung@lldevgt-sleung2.office.globalrelay.net>
2024-01-30 17:46:10 -0800

    VA-6765 add logs

--- apis/sms-gateway/sms-gateway-controller.ts
+++ apis/sms-gateway/sms-gateway-controller.ts
@@ -1,3 +1,4 @@
+import { Log } from 'Apis/api-helpers/log-utils';
 import { Client } from 'cassandra-driver';
 
 export class SmsGatewayController {
@@ -7,11 +8,13 @@ export class SmsGatewayController {
     }
 
     async getAccountSid(companyId: number) {
+        Log.info(`..request to SMS gateway Cassandra database to get account id from company '${companyId}'`);
         const result = await this.client.execute(
             `SELECT account_id FROM company_account WHERE company_id = ${companyId};`
         );
         const firstRow = result.first();
         const values = firstRow.values();
+        Log.success(`obtained accountSid '${values}' from companyId '${companyId}'`);
         return values[0];
     }
 }
