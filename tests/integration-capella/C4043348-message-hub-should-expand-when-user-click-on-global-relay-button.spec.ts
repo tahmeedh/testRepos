e1a4666e873888dfe120b706d0dd392b7be962fe
Rhythm <rhythm.vashit@globalrelay.net>
2024-04-01 17:21:30 -0700

    CA-1387 removed teardown

--- tests/integration-capella/C4043348-message-hub-should-expand-when-user-click-on-global-relay-button.spec.ts
+++ tests/integration-capella/C4043348-message-hub-should-expand-when-user-click-on-global-relay-button.spec.ts
@@ -1,31 +1,28 @@
-import { test, chromium, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { Company } from 'Apis/company';
 import { TestUtils } from 'helper/test-utils';
 import { Log } from 'Apis/api-helpers/log-utils';
 import { BaseController } from 'Controllers/base-controller';
 
 const { testAnnotation, testName, testTags } = TestUtils.getTestInfo(__filename);
-let browser = null;
-let context1 = null;
+
 let app: BaseController;
 
 let company: Company;
 let user1 = null;
 
 test.beforeEach(async () => {
-    browser = await chromium.launch();
     company = await Company.createCompany();
     user1 = await company.createUser();
 });
 
-test(`${testName} ${testTags}`, async () => {
+test(`${testName} ${testTags}`, async ({ page }) => {
     test.info().annotations.push(testAnnotation);
     Log.starDivider(
         `START TEST: Create browser and login with ${user1.userInfo.firstName} ${user1.userInfo.lastName}`
     );
-    context1 = await browser.newContext();
-    const page1 = await context1.newPage();
-    app = new BaseController(page1);
+
+    app = new BaseController(page);
     await app.goToLoginPage();
     await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
 
@@ -47,11 +44,7 @@ test(`${testName} ${testTags}`, async () => {
     await expect(app.startChatButtonController.Pom.START_CHAT).toBeVisible();
 
     // Verify Welcome text is shown
-    await expect(app.messageHubController.Pom.WELCOME_TEXT).not.toBeVisible();
+    await expect(app.messageHubController.Pom.WELCOME_TEXT).toBeVisible();
 
     Log.starDivider(`END TEST: Test Execution Commpleted`);
-});
-
-test.afterEach(async () => {
-    await company.teardown();
 });
