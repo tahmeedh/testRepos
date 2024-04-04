846d352a3f334959e3e750e3675dfd70e95b009e
Sam Leung <sam.leung@lldevgt-sleung2.office.globalrelay.net>
2024-04-04 15:24:23 -0700

    VA-6935 add grcp controllers and add tests for concurrent sessions mute

--- apis/grcp/grcp-base-controller.ts
+++ apis/grcp/grcp-base-controller.ts
@@ -3,7 +3,6 @@ import { Page } from '@playwright/test';
 export class GrcpBaseController {
     static async getRequest(page: Page, msgData) {
         // Since the first param returns a function and we need the second param to pass in the data. pageFunction is just an unused variable.
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         await page
             .frameLocator('#message-iframe')
             .locator(':root')
