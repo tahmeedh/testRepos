1738c4652d0991d51a7a3913ecc1bbd69bb009ce
Sam Leung <sam.leung@lldevgt-sleung2.office.globalrelay.net>
2024-07-18 14:32:22 -0700

    VA-7408 refactor and fix test

--- tests/chat-window/sms/back-button/C3812529-SMS-user-can-open-company-vCard-from-create-view-and-return-to-message-hub.spec.ts
+++ tests/chat-window/sms/back-button/C3812529-SMS-user-can-open-company-vCard-from-create-view-and-return-to-message-hub.spec.ts
@@ -1,92 +1,94 @@
 import { test, expect } from '@playwright/test';
-import { Company } from 'Apis/company';
 import { TestUtils } from 'helper/test-utils';
 import { BaseController } from 'Controllers/base-controller';
-import { Log } from 'Apis/api-helpers/log-utils';
-import { User } from 'Apis/user';
+import { users } from 'Constants/users';
 
 const { testAnnotation, testName, testTags } = TestUtils.getTestInfo(__filename);
 let app: BaseController;
 
-let company: Company;
-let user1: User = null;
-let user2: User = null;
-
-test.beforeAll(async () => {
-    company = await Company.createCompany();
-    user1 = await company.createUser();
-    user2 = await company.createUser();
-    await Promise.all([
-        user1.assignServiceManagerRole('MESSAGE_ADMINISTRATOR'),
-        user1.assignDirectoryRole('SMS_USER_WITH_CALL_FORWARD')
-    ]);
-
-    await user1.requestAndAssignTwilioNumber();
-});
+const user1 = users.VCARD_1;
 
 test(`${testName} ${testTags}`, async ({ page }) => {
     test.info().annotations.push(testAnnotation);
-    Log.starDivider(`START TEST`);
     app = new BaseController(page);
 
-    await test.step('GIVEN - User has an existing SMS conversation', async () => {
+    await test.step('GIVEN', async () => {
         await test.step('Go to login page', async () => {
             await app.goToLoginPage();
         });
 
         await test.step('Login', async () => {
-            await app.loginController.loginToPortal(user1.userInfo.email, user1.userInfo.password);
+            await app.loginController.loginToPortal(user1.EMAIL, user1.PASSWORD);
             await app.portalController.closeEnableDesktopNotification();
-            await app.newsAlertController.clickNextSMSEnabledNotification();
-            await app.portalController.clickCloseSMSEnabledNotification();
-        });
-    });
-
-    await test.step('User can navigate to company vCard in SMS create', async () => {
-        await test.step(`User clicks on 'start-new-chat' button`, async () => {
-            await app.startChatButtonController.ClickOnStartSMS();
         });
 
         await test.step(`User 1 searches for user 2`, async () => {
-            await app.createChatController.SearchSMSUser(
-                `${user2.userInfo.firstName} ${user2.userInfo.lastName}`
-            );
+            await app.hubHeaderController.clickStartChatButton();
+            await app.hubHeaderController.selectHeaderMainMenuOption('Text');
+            await app.createChatController.SearchSMSUser('vCard 2');
+        });
+    });
+
+    await test.step('STEP1. User can open vCard', async () => {
+        await test.step('WHEN', async () => {
+            await app.createChatController.clickRowAvatarByPhoneNumber('+1 604 263 2598');
         });
 
-        await test.step(`User 1 clicks on the avatar of the first result`, async () => {
-            await app.createChatController.clickOnSearchComponentRowAvatar(
-                `${user2.userInfo.firstName} ${user2.userInfo.lastName}`
-            );
-            await expect(app.vCardController.Pom.COMPANY_NAME_INTERNAL).toHaveText(
-                `${user2.userInfo.company.companyName}`
+        await test.step('THEN', async () => {
+            await expect(app.vCardController.Pom.COMPANY_NAME_INTERNAL, `vCard is visible`).toHaveText(
+                `Vega Static Company`
             );
         });
+    });
 
-        await test.step('Company vCard is displayed when user clicks on company name', async () => {
+    await test.step('STEP2. User can open company vCard', async () => {
+        await test.step('WHEN', async () => {
             await app.vCardController.Pom.COMPANY_NAME_INTERNAL.click();
-            await expect(app.companyVCardController.Pom.COMPANY_NAME).toHaveText(
-                `${user2.userInfo.company.companyName}`
+        });
+
+        await test.step('THEN', async () => {
+            await expect(app.companyVCardController.Pom.COMPANY_NAME, `Company vCard is visible`).toHaveText(
+                `Vega Static Company`
             );
         });
     });
 
-    await test.step('User can navigate to back to message hub from company vCard', async () => {
-        await test.step('User vCard is displayed when user clicks on back button', async () => {
+    await test.step('STEP3. User can return to user vCard', async () => {
+        await test.step('WHEN', async () => {
             await app.companyVCardController.clickOnBackButton();
-            await expect(app.vCardController.Pom.COMPANY_NAME_INTERNAL).toHaveText(
-                `${user2.userInfo.company.companyName}`
-            );
         });
 
-        await test.step('Create view is displayed when user clicks on back button', async () => {
-            await app.vCardController.clickOnBackButton();
-            await expect(app.createChatController.Pom.EXTERNAL_SEARCH_INPUT).toBeVisible();
-        });
-
-        await test.step('Message hub displayed when user clicks on cancel button', async () => {
-            await app.createChatController.clickOnCancelButton();
-            await expect(app.startChatButtonController.Pom.START_CHAT).toBeVisible();
+        await test.step('THEN', async () => {
+            await expect(
+                app.vCardController.Pom.COMPANY_NAME_INTERNAL,
+                `Company vCard is visible`
+            ).toHaveText(`Vega Static Company`);
         });
     });
-    Log.starDivider(`END TEST`);
+
+    await test.step('STEP3. User can return to create view', async () => {
+        await test.step('WHEN', async () => {
+            await app.vCardController.clickOnBackButton();
+        });
+
+        await test.step('THEN', async () => {
+            await expect(
+                app.createChatController.Pom.EXTERNAL_SEARCH_INPUT,
+                'User vCard is visible'
+            ).toBeVisible();
+        });
+    });
+
+    await test.step('STEP4. User can return to message hub', async () => {
+        await test.step('WHEN', async () => {
+            await app.createChatController.clickOnCancelButton();
+        });
+
+        await test.step('THEN', async () => {
+            await expect(
+                app.startChatButtonController.Pom.START_CHAT,
+                'message hub start chat button is visible'
+            ).toBeVisible();
+        });
+    });
 });
