f39acc5f99e31b13a807d47ec9edcd4c76655f48
Sam Leung <sam.leung@lldevgt-sleung2.office.globalrelay.net>
2024-07-19 14:29:28 -0700

    VA-7408 replace try catch with promise.catch

--- helper/ignore-error-utils.ts
+++ helper/ignore-error-utils.ts
@@ -2,7 +2,7 @@ import { Log } from 'Apis/api-helpers/log-utils';
 
 export class IgnoreErrorUtils {
     /**
-     * Allow an action to fail and catches its error, to prevent it from failing tests.
+     * Allow an action to fail and catches its error, to prevent it from failing test.
      * This is useful in scenarios where SMS/WA/Portal/Desktop notification is not displayed consistently.
      * Notes: Wrap the action inside an anonymous function to prevent action from being triggered when passing an action with a parameter.
      * @example
@@ -10,7 +10,7 @@ export class IgnoreErrorUtils {
      * @param action The method you want to continue to run when an error occur.
      * @returns Promise <void>
      */
-    static ignoreError(action: any): Promise<void> {
+    static ignoreError(action: () => Promise<void>) {
         return action().catch((error) => {
             Log.error('An error occurred.', error);
             Log.highlight('Ignoring error and continue to next step.');
