557a4238b07dd010e4a1106e64b4c7ca0e7f1dc2
Sam Leung <sam.leung@lldevgt-sleung2.office.globalrelay.net>
2024-07-26 17:28:54 -0700

    Revert "VA-7452 revert to older allure"
    This reverts commit b20aeb2138221b50a6caf0a08a2546f8bd3e4e5b.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, devices } from '@playwright/test';
-import { testPlanFilter } from 'allure-playwright/dist/testplan';
 
 /**
  * Read environment variables from file.
@@ -27,7 +26,6 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    grep: testPlanFilter(),
     reporter: [['html', { open: 'never' }], ['list'], ['allure-playwright']],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
