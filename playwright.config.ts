b20aeb2138221b50a6caf0a08a2546f8bd3e4e5b
Sam Leung <sam.leung@lldevgt-sleung2.office.globalrelay.net>
2024-07-26 17:21:23 -0700

    VA-7452 revert to older allure

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import { testPlanFilter } from 'allure-playwright/dist/testplan';
 
 /**
  * Read environment variables from file.
@@ -26,6 +27,7 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    grep: testPlanFilter(),
     reporter: [['html', { open: 'never' }], ['list'], ['allure-playwright']],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
