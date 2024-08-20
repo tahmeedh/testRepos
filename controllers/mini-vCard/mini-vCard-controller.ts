500fea11bbca0b46d9b5185623afa2b9dd60c9cd
Sam Leung <sam.leung@lldevgt-sleung2.office.globalrelay.net>
2024-08-20 13:34:44 -0700

    VA-7390 update steps

--- controllers/mini-vCard/mini-vCard-controller.ts
+++ controllers/mini-vCard/mini-vCard-controller.ts
@@ -1,6 +1,6 @@
 import { test, type Page } from '@playwright/test';
 import { Log } from 'Apis/api-helpers/log-utils';
-import { MiniVCardPage } from 'Poms/mini-vCard/mini-v-card-page';
+import { MiniVCardPage } from 'Poms/mini-vCard/mini-vcard-page';
 
 export class MiniVCardController {
     readonly page: Page;
@@ -19,22 +19,22 @@ export class MiniVCardController {
     }
 
     async clickMoreInfoBtn() {
-        await test.step(`Mini-VCard Controller: Click on 'message' button`, async () => {
-            Log.info(`Mini-VCard Controller: Click on 'message' button`);
+        await test.step(`Mini-VCard Controller: Click on 'more info' button`, async () => {
+            Log.info(`Mini-VCard Controller: Click on 'more info' button`);
             await this.Pom.MORE_INFO_BUTTON.click();
         });
     }
 
     async clickCtaSMSBtn() {
-        await test.step(`Mini-VCard Controller: Click on 'message' button`, async () => {
-            Log.info(`Mini-VCard Controller: Click on 'message' button`);
+        await test.step(`Mini-VCard Controller: Click on 'CTA SMS' button`, async () => {
+            Log.info(`Mini-VCard Controller: Click on 'CTA SMS' button`);
             await this.Pom.CTA_SMS_BUTTON.click();
         });
     }
 
     async clickCtaWABtn() {
-        await test.step(`Mini-VCard Controller: Click on 'message' button`, async () => {
-            Log.info(`Mini-VCard Controller: Click on 'message' button`);
+        await test.step(`Mini-VCard Controller: Click on 'CTA WhatsApp' button`, async () => {
+            Log.info(`Mini-VCard Controller: Click on 'CTA WhatsApp' button`);
             await this.Pom.CTA_WA_BUTTON.click();
         });
     }
