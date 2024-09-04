2988a6758b025cb60a97676bf959cb3989b791fe
Tahmeed Hossain <tahmeed.hossain@globalrelay.net>
2024-09-04 09:46:09 -0700

    VA-7592 use locator

--- poms/message-hub/news-alert-page.ts
+++ poms/message-hub/news-alert-page.ts
@@ -10,6 +10,8 @@ export class NewsAlertPage extends BasePage {
         super(page);
 
         this.NEWS_ALERT_NEXT_BUTTON = this.MESSAGEIFRAME.locator('.m-auto-news-alert-next-link');
-        this.NEW_FEATURE_TOOLTIP_CLOSE_BUTTON = this.MESSAGEIFRAME.locator('[data-id="close-sm"]');
+        this.NEW_FEATURE_TOOLTIP_CLOSE_BUTTON = this.MESSAGEIFRAME.locator(
+            '.m-auto-new-feature-tooltip-close'
+        );
     }
 }
